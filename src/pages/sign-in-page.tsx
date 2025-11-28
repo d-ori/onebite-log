4fcd326445ae38fb8a76a338f1a2a11ad435d54b
d-ori <jiographie@gmail.com>
2025-11-28 11:24:05 +0900

    feat: 로그인 에러시 토스트 메시지 처리

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -10,7 +10,11 @@ export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithPassword } = useSignInWithPassword({
+    onError: () => {
+      setPassword("");
+    },
+  });
   const { mutate: signInwithOAuth } = useSignInWithOAuth();
 
   const handleSignInClick = () => {
