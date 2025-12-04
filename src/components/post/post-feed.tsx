be05906a6c5ab6ce177a33bf975fc75c06442940
d-ori <jiographie@gmail.com>
2025-12-04 13:14:53 +0900

    feat: 포스트 무한 스크롤 기능 구현

--- src/components/post/post-feed.tsx
+++ src/components/post/post-feed.tsx
@@ -1,19 +1,32 @@
-import { usePostsData } from "@/hooks/queries/use-post-data";
 import PostItem from "./post-item";
 import Fallback from "../fallback";
 import Loader from "../Loader";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useInfinitePostsData } from "@/hooks/queries/use-infinite-posts-data";
 
 export default function PostFeed() {
-  const { data, error, isPending } = usePostsData();
+  const { data, error, isPending, isFetchingNextPage, fetchNextPage } =
+    useInfinitePostsData();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   if (error) return <Fallback />;
   if (isPending) return <Loader />;
 
   return (
     <div className="flex flex-col gap-10">
-      {data.map((post) => (
-        <PostItem key={post.id} {...post} />
-      ))}
+      {data.pages.map((page) =>
+        page.map((post) => <PostItem key={post.id} {...post} />),
+      )}
+      {isFetchingNextPage && <Loader />}
+
+      <div ref={ref}></div>
     </div>
   );
 }
