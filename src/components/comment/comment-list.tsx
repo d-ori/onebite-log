39f8f894b7c10d7cb76db3ac238fcb473bdc15dc
d-ori <jiographie@gmail.com>
2025-12-10 17:47:17 +0900

    feat: 댓글 리스트 조회 기능 구현

--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
+import { useCommentData } from "@/hooks/queries/use-comments-data";
 import CommentItem from "./comment-item";
+import Fallback from "../fallback";
+import Loader from "../loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchCommentsPending,
+  } = useCommentData(postId);
+
+  if (fetchCommentsError) return <Fallback />;
+  if (isFetchCommentsPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
